310399e98ebcfd27b71355c8ca0fdf883ce9dbd9
Dixit Parmar <Dparmar006@users.noreply.github.com>
2021-01-15 19:02:01 +0530

    LOGIN URL WORKS

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import InfoScreen from "./screens/InfoScreen";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import App from "./App";
+import Login from "./screens/Login";
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={App} />
         <Route path="/playlist" component={InfoScreen} exact />
+        <Route path="/login" component={Login} exact />
       </Switch>
     </BrowserRouter>
   );
