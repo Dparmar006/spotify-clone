ebb99ff902b6e68fe6caa88ffe744e5073e73fa8
Dixit Parmar <Dparmar006@users.noreply.github.com>
2021-01-20 17:37:48 +0530

    auth conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Row from "./Components/Row";
+import Login from "./screens/Login";
+import { getAccessTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getAccessTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify
+        .getMe()
+        .then((user) => console.log(user))
+        .catch((error) => console.log(error));
+    }
+  }, []);
+  console.log(token);
   return (
     <div className="app">
-      <Header />
-      <Row />
-      <Row />
-      <Row />
+      {token ? (
+        <>
+          <Header />
+          <Row />
+          <Row />
+          <Row />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -15,3 +15,14 @@ export const scopes = [
 export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
+
+export const getAccessTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, items) => {
+      let parts = items.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
